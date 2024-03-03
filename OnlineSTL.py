02594b093846c024552df513bb316491d7a843cf
yehengyu <yehengyu@bytedance.com>
2024-03-03 16:28:28 +0800

    init commit

--- OnlineSTL.py
+++ OnlineSTL.py
@@ -1,46 +1,6 @@
 import numpy as np
 import math
-
-
-def W(u): # The tri-cube kernel
-    if u >= 0 and u < 1:
-        return (1 - u ** 3) ** 3
-    else:
-        return 0
-def TrendFilter(X):
-    lam = len(X)
-    k_lam = (lam - 1 - np.arange(lam)) / lam  # weight vector
-    for i in range(lam):
-        k_lam[i] = W(k_lam[i])
-    y = np.sum(k_lam * X) / np.sum(k_lam)
-    return y
-
-def SymTrendFilter(X, m_p):
-    trend = np.zeros(len(X))
-    for i in range(len(X)):
-        left = max(0, i - m_p + 1)
-        right = min(len(X) - 1, i + m_p - 1)
-        lam = right - left + 1
-        k_lam = np.abs((np.arange(lam) - (i - left))) / lam
-        for j in range(lam):
-            k_lam[j] = W(k_lam[j])
-        trend[i] = np.sum(k_lam * X[left: right + 1]) / np.sum(k_lam)
-    return trend
-
-
-def SeasonalityFilter(D, m, lam):
-    # Return the seasonal part of period m
-    C_D = np.zeros(len(D))
-    for k in range(m):
-        c = D[k]
-        i = 0
-        while True:
-            C_D[k + i * m] = c
-            i += 1
-            if k + i * m >= len(D):
-                break
-            c = lam * D[k + i * m] + (1 - lam) * c
-    return C_D
+from filters import TrendFilter, SymTrendFilter, SeasonalityFilter
 
 
 # Ensure that the input length is larger than 4 * m (m represents the largest period)
@@ -75,7 +35,12 @@ class OnlineSTL:
         array = np.append(array, x)
         array = np.delete(array, 0)
         return array
+
     def update(self, x):
+        # Input: x -- value of new coming point
+        # Output: T -- Trend 
+        # S -- Seasonal
+        # R -- Remainder 
         self.A = self.UpdateArray(self.A, x)
         b = x
         S = []
